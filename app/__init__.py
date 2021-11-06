67723d8639253ece452d3b42e4ffd6b18e2e963d
sean-gall-41 <galloglyst149@gmail.com>
2021-11-05 23:45:27 -0500

    user login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from config import Config
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
